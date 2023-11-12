114cc0891ab74ce885ed0fe5867631f4c89f5847
jefsotimao <jefersonc2012@gmail.com>
2023-11-12 00:57:52 -0300

    Criando ação ao clicar na imagem para trazer seu resumo

--- script.js
+++ script.js
@@ -2,3 +2,25 @@ var map = L.map('map').setView([-25.45318843660639, -49.53415404516462], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
 }).addTo(map);
+
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+if(tabMenu.length && tabContent.length)
+{
+    tabContent[0].classList.add('ativo');
+
+    function activeTab(index){
+        tabContent.forEach((section) =>{
+            section.classList.remove('ativo');
+        });
+
+        tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu,index) =>{
+        itemMenu.addEventListener('click', () =>{
+            activeTab(index);
+        });
+    })
+}
